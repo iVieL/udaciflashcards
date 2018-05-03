9e032d920db8f0d7e835ba871eda792b0c856999
Victor Lutin <victorl@autobytel.com>
2018-05-03 09:28:22 -0600

    lineups+

--- App.js
+++ App.js
@@ -9,12 +9,20 @@ import { StyleSheet, Text, View } from 'react-native';
     - Handle Decks: user can have more than one category
     - Handle quizzes on those decks
 
+    Specification:
+    - Allow users to create a deck which can hold an unlimited number of cards
+    - Allow users to add a card to a specific deck
+    - The front of the card should display the question
+    - The back of the card should display the answer
+    - Users should be able to quiz themselves on a specific deck and receive a score once they're done.
+    - Users should receive a notification to remind themselves to study if they haven't already for that day.
+
     Views:
     1. Deck list view (default view)
         1.1. Display the title of each Deck
         1.2. Display the number of cards in each deck
     2. Individual deck view
-        2.1. Dispaly title of the deck
+        2.1. Display title of the deck
         2.2. display the number of cards in the deck
         2.3. display an option to start a quiz on this specific deck
         2.4. An option to add a new question to the deck
